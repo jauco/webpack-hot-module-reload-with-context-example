a670b83c8918c60dd6a1eaedea5b2d71fab7ee8d
Jauco Noordzij <code@jauco.nl>
2016-07-01 14:28:01 +0200

    launch the HMR and change the export of `a.js`
    The callback will never be called because hmr_example already accepts it

--- entry.js
+++ entry.js
@@ -1,13 +1,8 @@
 var modules = require("./hmr_example").modules;
-printModuleKeys(modules);
 
-if(module.hot) {
-    module.hot.accept("./hmr_example", function() {
-        console.log("detected change to modules from entry.js");
-        printModuleKeys(require("./hmr_example").modules);
-    });
+function printModuleContents() {
+    console.log(JSON.stringify(modules));
 }
 
-function printModuleKeys(modules) {
-    console.log(Object.keys(modules));
-}
+printModuleContents();
+window.setInterval(function () { printModuleContents(); }, 2000);
--- filesToLoad/a.js
+++ filesToLoad/a.js
@@ -1,1 +1,2 @@
 console.log("a");
+module.exports.foo = "change me!"
